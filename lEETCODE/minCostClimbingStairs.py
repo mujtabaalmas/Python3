ba08bcec0aac5cdab77545b2faf372f4aeafa9fd
mujtabaalmas <kingmujtaba30@gmail.com>
2025-07-08 17:45:06 +0500

    updating file

--- lEETCODE/minCostClimbingStairs.py
+++ lEETCODE/minCostClimbingStairs.py
@@ -1,5 +1,11 @@
 #minium cost for climbling stairs ------------
-# suppose we have cost i and ith index is the climbling cost of stairs we have to find the minimum cost first we take len of cost list then pass two check whether its length is equal to 0 or 1 and return 0 and 0th index respectively. then initialize our dp list we (we build another dp list where minimum cost id dp[i] ) we have initialize it with length of cost list and its 0th and 1st index with cost of 0 and first index repectively. then a for loop which starts from index 2 because we already initiazes value 0,1 and assign to dp array then for loop goes to n -1 iteration we apply algorithm ith -1 index and ith-2 index take minimum from it and add it to current cost and store it in dp array return the minimum of n-2 and n-1 from dp dp array that is the minimum cost.
+# suppose we have cost i and ith index is the climbling cost of stairs we have to find the minimum cost first
+#  we take len of cost list then pass two check whether its length is equal to 0 or 1
+#  and return 0 and 0th index respectively. then initialize our dp list we (we build another dp list where minimum cost id dp[i] )
+#  we have initialize it with length of cost list and its 0th and 1st index with cost of 0 and first index repectively.
+#  then a for loop which starts from index 2 because we already initiazes value 0,1 and assign to dp array then for loop goes to n -1 iteration 
+# we apply algorithm ith -1 index and ith-2 index take minimum from it and add it to current cost and store it in dp array return the minimum of n-2 and n-1  
+# from dp array that is the minimum cost.
 class Solution:
     def minCostClimbingStairs(self, cost: list[int]) -> int:
         print(cost)
